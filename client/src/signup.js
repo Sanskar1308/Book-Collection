4ba7d1170adb3148eda5477a7bc8d4f48fa81976
Sanskar Chirania <sanskar125112@gmail.com>
2024-06-12 17:06:37 +0530

    updated signup styling also

--- client/src/signup.js
+++ client/src/signup.js
@@ -8,21 +8,23 @@ function Signup() {
   const [name, setName] = useState("");
   const navigate = useNavigate();
 
-  async function handleSignup() {
+  async function handleSignup(event) {
+    event.preventDefault(); // Prevent default form submission
+
     try {
       const response = await fetch("http://localhost:3001/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ name: name, email: email, password: password }), // Need to stringify the body
+        body: JSON.stringify({ name: name, email: email, password: password }),
       });
 
       if (response.ok) {
-        // Navigate to the dashboard after successful login
+        // Navigate to the login page after successful signup
         navigate("/login");
       } else {
-        // SignIn failed, display error message
+        // Signup failed, display error message
         console.error("Signup failed:", await response.text());
       }
     } catch (error) {
@@ -31,37 +33,59 @@ function Signup() {
   }
 
   return (
-    <>
-      <div>
-        <input
-          type="text"
-          placeholder="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="btn btn-2" onClick={handleSignup}>
-          Signup
-        </button>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-96 backdrop-blur-lg bg-opacity-80 rounded-lg shadow-lg p-5 bg-gray-900 text-white ">
+        <h2 className="text-2xl font-bold pb-5">SignUp</h2>
+        <form onSubmit={handleSignup}>
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-medium">Your name</label>
+            <input
+              className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"
+              type="text"
+              placeholder="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-medium">Your email</label>
+            <input
+              className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-medium">
+              Your password
+            </label>
+            <input
+              className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center justify-between mb-4">
+            <button
+              className="text-white bg-purple-600 hover:bg-purple-700 focus:ring-2 focus:ring-blue-300 font-medium rounded-lg text-sm py-2.5 px-5 w-full sm:w-auto"
+              type="submit"
+            >
+              Signup
+            </button>
+            <div className="flex items-center text-sm">
+              <p>Already have an account?</p>
+              <Link to="/login">
+                <p className="underline cursor-pointer ml-1">Sign in</p>
+              </Link>
+            </div>
+          </div>
+        </form>
       </div>
-      <div>
-        <h3>Already a user?</h3>
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-      </div>
-    </>
+    </div>
   );
 }
 
